3d2a6542289e1ac4c0f3ebe42d9e40772a375466
Sebastien Maloron <contact@smaloron.eu>
2021-04-21 06:02:34 +0200

    Gestion des listes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
+// Importation des composants pour l'affichage 
+// et la gestion de l'écran
 import { Component } from '@angular/core';
 
+// Décorateur
+// permet d'indiquer le rôle de l'élément qui suit
+// et de le paramètre
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public name = 'Seb';
+
+  public preferedColor = 'warning';
+
+  public isListHidden = true;
+
+  public countryList = [
+    { name: 'France', mainCity: 'Paris', flag: 'french-flag.jpg', population: 67 },
+    { name: 'Irelande', mainCity: 'Dublin', flag: 'irish-flag.jpg', population: 5 },
+    { name: 'Allemagne', mainCity: 'Berlin', flag: null, population: 85 }
+  ];
+
+  constructor() { }
+  
+  public showHideList() {
+    /*
+    if (this.isListHidden) {
+      this.isListHidden = false;
+    } else {
+      this.isListHidden = true;
+    } */
+    this.isListHidden = !this.isListHidden;
+  }
 
 }
